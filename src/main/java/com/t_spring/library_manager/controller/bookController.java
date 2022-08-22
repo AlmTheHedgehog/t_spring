bfd73b10f4e5d80dcb1f123731be0da7e883fdc3
AlmTheHedgehog <tbe6666@gmail.com>
2022-08-22 20:10:44 +0200

    added FindAllBooksOfAuthor

--- src/main/java/com/t_spring/library_manager/controller/bookController.java
+++ src/main/java/com/t_spring/library_manager/controller/bookController.java
@@ -1,7 +1,6 @@
 package com.t_spring.library_manager.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.t_spring.library_manager.exeption.ResourceNotFoundExeption;
@@ -29,6 +29,11 @@ public class bookController {
     @GetMapping("/books")
     public List<book> getAllBooks(){
         return bookRepository.findAll();
+    }
+
+    @RequestMapping("/books")
+    public List<book> getBooksOfAuthor(@RequestParam("author") String author){
+        return bookRepository.findAllByAuthor(author);
     }
 
     @PostMapping("/books")
--- src/main/java/com/t_spring/library_manager/repository/BookRepository.java
+++ src/main/java/com/t_spring/library_manager/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package com.t_spring.library_manager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.t_spring.library_manager.model.book;
 
 @Repository
 public interface BookRepository extends JpaRepository<book, Long> {
-    
+    List<book> findAllByAuthor(String author);
 }
